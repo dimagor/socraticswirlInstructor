f0d21566a25589665c82d6036fb3170b42031cfb
Dima Gorenshteyn <dima@nat-oitwireless-inside-vapornet100-c-30521.Princeton.EDU>
2015-03-29 17:37:07 -0400

    Time progress binning graph, minor cosmetic fixes

--- dashboard/server.R
+++ dashboard/server.R
@@ -4,62 +4,71 @@ library(ggplot2)
 library(swirl)
 
 shinyServer(function(input, output, session) {
-  
+
   # Static Definitions ----------
   getPctColor <- function(pct){
     typeColors = c("black","red","orange","yellow","light-blue","navy","teal","aqua","lime","olive","green")
     typeColors[round(as.numeric(pct) / 10) + 1]
   }
   lectureInfo <- Parse_retrieve("lecdb_dima")
-  
+
   # Reactive Functions ---------------
   usersLogged <- reactive({
     input$refresh #Refresh when button is clicked
     interval <- max(as.numeric(input$interval), 5)
     if(input$interval != FALSE) invalidateLater(interval * 1000, session)
     Parse_retrieve("udb_dima") %>% .$student %>% unique %>% length
-  })  
-  
+  })
+
   questionsAnswered <- reactive({
     input$refresh
     interval <- max(as.numeric(input$interval), 5)
     if(input$interval != FALSE) invalidateLater(interval * 1000, session)
     Parse_retrieve("adb_dima") %>% group_by()
-  })  
-  
+  })
+
   lastUpdateTime <- reactive({
     usersLogged()
     Sys.time()
   })
-  
+
   selectedExercise <- reactive({
     questionsAnswered() %>% filter(exercise == input$exerciseID)
   })
-  
+
+  exerciseTemporalTable <- reactive({
+    exercise <- selectedExercise()
+    data.frame(t_cut = seq(min(exercise$updatedAt), max(exercise$updatedAt) + 30, by = '30 sec')) %>%
+      group_by(t_cut) %>%
+      mutate(num_attempts = sum(distinct(group_by(exercise,student))$updatedAt <= t_cut),
+             num_correct = sum(filter(distinct(group_by(exercise,student)),correct)$updatedAt <= t_cut)) %>%
+      gather(type,num,-t_cut) %>% mutate(pct = num/usersLogged() * 100)
+  })
+
   # Header --------
-  
+
   output$progressMenu <- renderMenu({
     progress_breakdown <- questionsAnswered() %>%
       group_by(exercise) %>%
       distinct(student, exercise, correct) %>%
-      summarise(n=sum(correct)) %>% 
+      summarise(n=sum(correct)) %>%
       mutate(pct=round(n / usersLogged(), 2)*100)
     progress_breakdown <- left_join(lectureInfo,progress_breakdown, by="exercise") %>%
       mutate(pct=ifelse(is.na(pct),0,pct))
     progress_msgs <- apply(progress_breakdown, 1, function(row) {
-      taskItem(value = row[["pct"]], 
+      taskItem(value = row[["pct"]],
                color = getPctColor(row[["pct"]]),
                paste("Exercise:", row[["exercise"]])
       )
     })
-    
+
     dropdownMenu(type = "tasks", .list = progress_msgs)
   })
   # Sidebar --------------
   output$usersessions <- renderUI({
-    h3("Sessions:", as.character(usersLogged()))
+      h3("Sessions:", as.character(usersLogged()))
   })
-  
+
   output$timeSinceLastUpdate <- renderUI({
     # Trigger this every 5 seconds
     invalidateLater(5000, session)
@@ -70,8 +79,8 @@ shinyServer(function(input, output, session) {
       " seconds ago."
     )
   })
-  
-  
+
+
   # BODY ------------
   output$selectExercise <- renderUI({
     exercises = as.list(lectureInfo$exercise)
@@ -83,44 +92,53 @@ shinyServer(function(input, output, session) {
     attempted_pct = round(attempted/usersLogged() * 100)
     taskItem(paste("Attempted:", attempted) , value = attempted_pct, color = getPctColor(attempted_pct))
   })
-  
+
   output$completedBar <- renderUI({
     completed = selectedExercise() %>% filter(correct) %>% distinct(student) %>% nrow
     completed_pct = round(completed/usersLogged() * 100)
     taskItem(paste("Completed:", completed) , value = completed_pct, color = getPctColor(completed_pct))
   })
-  
+
   output$exerciseQuestion <- renderText(
     lectureInfo %>% filter(exercise == input$exerciseID) %>% .$description
   )
-  
+
   output$exerciseAnswer <- renderText(
     lectureInfo %>% filter(exercise == input$exerciseID) %>% .$desired_answer
   )
-  
+
   output$incorrectAnswers <- renderDataTable(
     selectedExercise() %>% filter(!correct) %>% count(Answer=answer) %>% arrange(-n)
   )
-  
+
   #TODO: Fun placeholder, make something sensible
   #NOTES: add switch dropdown for multiple plots, or consider gridextra
   output$exerciseGraph <- renderPlot({
     exercise_data <- selectedExercise()
-    if(nrow(exercise_data)==0) NULL
+    if(nrow(exercise_data) == 0 ) NULL
     else{
+      exercise_tt <- exerciseTemporalTable()
       switch(input$exerciseGraphSelect,
-             "attemptbar" = selectedExercise() %>% count(student) %>%
+             "attemptbar" = exercise_data %>% count(student) %>%
                count(attempts=factor(n)) %>%
-               ggplot(aes(x = attempts, y = n, fill = attempts)) + 
-               geom_bar(stat = "identity") + 
+               ggplot(aes(x = attempts, y = n, fill = attempts)) +
+               geom_bar(stat = "identity", fill = "#6495ED") +
                coord_flip() +
-               theme_classic() + 
-               xlab("Attempts") + ylab("Frequency") + 
+               theme_minimal() +
+               xlab("Attempts") + ylab("Frequency") +
                guides(fill = FALSE) +
-               scale_fill_brewer()
+               scale_fill_brewer(),
+             "timetracking" = ggplot(exercise_tt,aes(t_cut,pct, color = type)) +
+               geom_path(cex=2) + ylim(0,100) +
+               #10 minutes since first attempt or greater
+               xlim(min(exercise_tt$t_cut),max(min(exercise_tt$t_cut) + 600,max(exercise_tt$t_cut))) +
+               ylab("% of Students") + xlab("") +
+               scale_color_discrete(name = "" ,labels = c("Attempted", "Completed")) +
+               theme_classic() +
+               theme(legend.justification=c(1,0), legend.position=c(1,0))
       )
     }
-    
+
   })
-  
+
 })
---
+++ tmp/example_data.R
@@ -0,0 +1,76 @@
+library(digest)
+source("~/.Rprofile")
+
+#
+# # AnswerDB
+# #
+# # adb_dima
+# #
+# # Fields: course, lesson, instructor, student, exercise, correct, answer
+
+# # Lecture DB --------------
+# swirl:::Parse_create("lecdb_dima",course="default",lesson="ggplot", instructor="dima", exercise=1, description="Load ggplot library", desired_answer="library(ggplot2)")
+# swirl:::Parse_create("lecdb_dima",course="default",lesson="ggplot", instructor="dima", exercise=2, description="Count the diamonds db by color", desired_answer="count(diamonds,color)")
+# swirl:::Parse_create("lecdb_dima",course="default",lesson="ggplot", instructor="dima", exercise=3, description="Plot diamonds: carat vs price", desired_answer="ggplot(diamonds,aes(carat,price))+geom_point()")
+# swirl:::Parse_create("lecdb_dima",course="default",lesson="ggplot", instructor="dima", exercise=4, description="Plot diamonds: carat vs price with color breakdown", desired_answer="ggplot(diamonds,aes(carat,price,color=color))+geom_point()")
+
+
+#
+# # Participant DB
+# #
+# # swirl:::Parse_create()
+# #
+# # udb_dima
+# #
+# # Fields: course, lesson, instructor
+#
+
+# for(i in seq(25)) swirl:::Parse_create("udb_dima",course="default",lesson="ggplot", instructor="dima", student=digest(i))
+
+
+simulateStudents <- function(answerpool,  #Assume first is correct
+                             exercise = 1,
+                             course = "default",
+                             lesson = "ggplot",
+                             instructor = "dima",
+                             totalstudents = 25,
+                             sampledstudents = 15,
+                             attemptbuffer = 2,
+                             maxsleep = 20){
+  for(s in sample(seq(totalstudents), sampledstudents)) {
+    for(a in seq(sample(length(answerpool) + attemptbuffer, 1))){ #attempts
+      answer = sample(answerpool, 1)
+      isCorrect = answer == answerpool[1]
+      swirl:::Parse_create("adb_dima",
+                           course = course,
+                           lesson = lesson,
+                           instructor = instructor,
+                           student = digest(s),
+                           exercise = exercise,
+                           correct = isCorrect,
+                           answer  = answer)
+      if(isCorrect) break
+      pause = sample(seq(1,maxsleep),1)
+      print(paste("Waiting ", as.character(pause)))
+      Sys.sleep(pause) #For time series graphs
+    }
+  }
+}
+
+
+q1_ans <- c("library(ggplot2)", "library(ggplot2)", #To improve chances
+            "libary(ggplot2)","library(ggplot)","library ggplot", "library gplot", "lib(ggplot2)")
+simulateStudents(answerpool = q1_ans, exercise = 1, maxsleep = 10)
+
+q2_ans <- c("count(diamonds,color)", "count(diamonds,color)",
+            "counts(diamonds, color)",
+            "count(diamonds)",
+            "count(diamonds, clarity)",
+            "for(i in factor(diamonds$color)) print(color)",
+            "ggplot(diamonds,aes(color,price)) + geom_boxplot()",
+            "count diamond,color",
+            "countit(diamond,color)",
+            "Up, Up, Down, Down, Left, Right, Left, Right, B, A, Start"
+            )
+
+simulateStudents(answerpool = q2_ans, exercise = 2, maxsleep = 12)
