24555fb467c37bf307fd9db843cf9e83c44ba243
dgrtwo <dgrtwo@princeton.edu>
2015-04-03 14:36:07 -0400

    bugfixes to retrieve functions

--- R/parse.R
+++ R/parse.R
@@ -5,11 +5,18 @@ base_URL <- "https://api.parse.com"
 Parse_headers <- function() {
   header_strs <- c('X-Parse-Application-Id' = Sys.getenv("PARSE_APPLICATION_ID"),
                    'X-Parse-REST-API-Key' = Sys.getenv("PARSE_API_KEY"))
-  
+
   if (any(header_strs == "")) {
     stop("DEVELOPMENT: must set PARSE_APPLICATION_ID and PARSE_API_KEY environment variables")
   }
-  
+
+  # if currently logged in:
+  user <- getOption("parse_user")
+  if (!is.null(user)) {
+    session_token <- user$sessionToken
+    header_strs <- c(header_strs, "X-Parse-Session-Token" = session_token)
+  }
+
   httr::add_headers(.headers = header_strs)
 }
 
@@ -25,7 +32,7 @@ Parse_POST <- function(path, body, ...) {
   req <- httr::POST(base_URL, path = paste0("1/", path),
                     body = rjson::toJSON(body),
                     encode = "json", Parse_headers(), ...)
-  
+
   process_Parse(req)
 }
 
@@ -34,17 +41,21 @@ Parse_POST <- function(path, body, ...) {
 process_Parse <- function(req) {
   txt <- httr::content(req, as = "text")
   j <- jsonlite::fromJSON(txt)
-  
+
+  if (!is.null(j$error)) {
+    stop("Error ", j$code, ": ", j$error)
+  }
+
   if ("results" %in% names(j)) {
     j <- j$results
   }
-  
+
   for (col in names(j)) {
     if (grepl("At$", col)) {
       j[[col]] <- as.POSIXct(j[[col]], origin = "1970-01-01", format="%Y-%m-%dT%H:%M:%S")
     }
   }
-  
+
   j
 }
 
@@ -63,13 +74,58 @@ Parse_create <- function(class_name, ...) {
 #' @export
 Parse_retrieve <- function(class_name, object_id, ...) {
   url <- paste("classes", class_name, sep = "/")
-  
+
   # as of now, accepts only exact queries
   params <- list(...)
   if (length(params) > 0) {
-    q <- list(where = rjson::toJSON())
+    q <- list(where = rjson::toJSON(params))
   } else {
     q <- NULL
   }
   Parse_GET(url, query = q)
+}
+
+
+#' create a new user
+#'
+#' @return a list comprising the user's session
+#'
+#' @export
+Parse_signup <- function(username, password, ...) {
+  ret <- Parse_POST("users/", list(username = username, password = password, ...))
+  options(parse_user = ret)
+  invisible(ret)
+}
+
+
+#' Log a user into Parse
+#'
+#' This submits a login
+#'
+#' @param username username
+#' @param password password
+#' @param ... additional arguments, such as email
+#'
+#' @return a list comprising the logged in user's session
+#'
+#' @export
+Parse_login <- function(username, password, ...) {
+  ret <- Parse_GET("login/", query = list(username = username, password = password))
+  options(parse_user = ret)
+  invisible(ret)
+}
+
+
+Parse_current_user <- function() {
+  Parse_GET("users/me")
+}
+
+
+#' reset a user's password (if they have email)
+#'
+#' @param email User's e-mail address
+#'
+#' @export
+Parse_password_reset <- function(email) {
+  Parse_POST("requestPasswordReset", email = email)
 }
--- R/socraticswirl-instructor.R
+++ R/socraticswirl-instructor.R
@@ -1,16 +1,39 @@
-#' log in an instructor
+#' sign up with an instructor account
+#'
+#' Sign up an account for teaching and uploading SocraticSwirl lessons.
+#' Note that this requires an email confirmation.
+#'
+#' @param username desired username
+#' @param password your password
+#' @param email e-mail address, which will sent a confirmation link
 #'
 #' @export
-socratic_swirl_instructor <- function(username, password, course = NULL) {
-  options(socratic_swirl_instructor = username,
-          socratic_swirl_course = course)
+socratic_swirl_signup <- function(username, password, email) {
+  Parse_signup(username, password, email = email)
 }
 
 
-#' open dashboard
+#' Log into an instructor account
+#'
+#' Log into an instructor account. This has to be done before accessing the
+#' SocraticSwirl dashboard or submitting a new course.
+#'
+#' @param username account username
+#' @param password account password
+#'
+#' @export
+socratic_swirl_instructor <- function(username, password) {
+  u <- Parse_login(username, password)
+  options(socratic_swirl_instructor = u$username)
+}
+
+
+#' Open the SocraticSwirl dashboard
+#'
+#' Opens the SocraticSwirl instructor dashboard in a browser.
 #'
 #' @export
 dashboard <- function() {
   app <- system.file("dashboard", package = "socraticswirlInstructor")
-  shiny::runApp(app)
+  shiny::runApp(app, launch.browser = TRUE)
 }
