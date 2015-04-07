dabc77ef61af138bdcb14bf69c29a661c4a503fd
dgrtwo <dgrtwo@princeton.edu>
2015-04-06 20:02:44 -0400

    Set application ID and API key in onAttach

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(...) {
+  Sys.setenv(PARSE_APPLICATION_ID = "C0pM75Sepnt5WhK6P6yhRA0TqVa6Xa3vqwZjpLfT",
+             PARSE_API_KEY = "HyXS1gEn6gf7gibjDJVWPYsnIoc0SXcp4mwohdmI")
+}
