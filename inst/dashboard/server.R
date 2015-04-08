c8b0cee4bd8bc8ad684855081a6c255b912cedc3
dgrtwo <dgrtwo@princeton.edu>
2015-04-08 18:06:37 -0400

    Added code for deploying to shinyapps

--- inst/dashboard/server.R
+++ inst/dashboard/server.R
@@ -4,6 +4,9 @@ library(tidyr)
 library(ggplot2)
 library(rparse)
 
+options(socratic_swirl_instructor = "demo")
+Sys.setenv(PARSE_APPLICATION_ID = "C0pM75Sepnt5WhK6P6yhRA0TqVa6Xa3vqwZjpLfT",
+           PARSE_API_KEY = "HyXS1gEn6gf7gibjDJVWPYsnIoc0SXcp4mwohdmI")
 
 # remove list columns from a table
 remove_df_columns <- function(tbl) {
