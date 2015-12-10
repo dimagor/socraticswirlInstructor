e54539b6be77708a603edf1a538fded89db31488
chaugustin <augustin@princeton.edu>
2015-12-10 16:11:35 -0500

    Added protype keys.R file.

---
+++ inst/dashboard/keys.R
@@ -0,0 +1,10 @@
+# keys for socraticswirl-instructor - login to database is handled in the program
+
+Sys.setenv(PARSE_APPLICATION_ID = "<parse.com application ID>", 
+PARSE_API_KEY = "<parse.com API key>")
+if (username == "default") {
+  username="<socratic swirl instructor username>"
+}
+if (password == "default") {
+password = "<socratic swirl instructor password>"
+}
