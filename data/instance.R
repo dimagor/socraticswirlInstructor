76c0fc7d31fdf53f144f3aae433faad1078c2618
HJ08003 <hubertj@princeton.edu>
2016-01-06 18:39:47 -0500

    prepare configuration files

--- data/instance.R
+++ data/instance.R
@@ -6,10 +6,6 @@ server_instance = "test"
 #
 # Please fill in the instructor account and password for the above server instance, created by socratic_swirl_signup
 #
-username = "mcahn"
-password = "allpolitics"
+username = "you username"
+password = "your password"
 
-#
-# socratic_swirl_instructor("mcahn", "allpolitics", instance="test")  # If test
-# socratic_swirl_instructor("mcahn", "2cardtricks")                   # If production
-#
--- data/keys.R
+++ data/keys.R
@@ -1,10 +1,10 @@
 # Fill in the application key and REST api key for the test server
 
-PARSE_APPLICATION_ID_TEST = 'ckXMHnjXCoHMq1JrM7r5nEQixJnZdcyOdTtSQBCd'
-PARSE_API_KEY_TEST = 'lJ20RxXJiLFpXE028c4gePlyoQsmGCjSQSNFyxxl'
+PARSE_APPLICATION_ID_TEST = 'TEST_APP_KEY'
+PARSE_API_KEY_TEST = 'TESTAPI_KEY'
 
 # Fill in the application key and REST api key for the production server
 
-PARSE_APPLICATION_ID_PROD = 'bf6u93D9Rxaeujxzkdedp3m5wVJNAoNMr9vRt5ab'
-PARSE_API_KEY_PROD = 'efh92r6hs0f8ESJcgOAY1i5ePmii86ip1QJRFTTK'
+PARSE_APPLICATION_ID_PROD = 'PROD_APP_KEY'
+PARSE_API_KEY_PROD = 'PROD_API_KEY'
 
