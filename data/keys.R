f64d275afaade124d63f7e289c9df2150f7c9eec
HJ08003 <hubertj@princeton.edu>
2016-01-06 18:44:55 -0500

    new files

---
+++ data/keys.R
@@ -0,0 +1,10 @@
+# Fill in the application key and REST api key for the test server
+
+PARSE_APPLICATION_ID_TEST = 'TEST_APP_KEY'
+PARSE_API_KEY_TEST = 'TESTAPI_KEY'
+
+# Fill in the application key and REST api key for the production server
+
+PARSE_APPLICATION_ID_PROD = 'PROD_APP_KEY'
+PARSE_API_KEY_PROD = 'PROD_API_KEY'
+
